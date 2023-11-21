7e42c04236eb33b14c94520b2b79242a754b149b
Yago Portela Silva de Vasconcelos <theypsv@gmail.com>
2023-11-21 17:06:44 -0300

    Add files via upload

--- PYTHON_SUPER_315_INFINITY/Aula_04/exercicio_02_aula4.py
+++ PYTHON_SUPER_315_INFINITY/Aula_04/exercicio_02_aula4.py
@@ -1,12 +1,15 @@
 def o_maior(n1,n2,n3):
     if n1 > n2 and n1 > n3:
-        return f"{n1} é o Maior!"
+        return f"O número {n1} é o Maior!"
     elif n2 > n1 and n2 > n3:
-        return f"{n2} é o Maior!"
+        return f"O número {n2} é o Maior!"
     elif n3 > n1 and n3 > n2:
-        return f"{n3} é o Maior!"
+        return f"O número {n3} é o Maior!"
     else:
         return "Há mais de 1 maior do mesmo valor!"
-    
-maior = int(input("Digite 3 números:"))
-print(o_maior(maior))
+list = [] 
+for i in range(1,4):
+    maior = int(input(f"Digite o {i}º número:"))
+    list.append(maior)
+
+print(o_maior(list[0],list[1],list[2]))
